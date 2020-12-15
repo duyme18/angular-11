b46513db17856e596b65dccd601f7c2f1c646bee
Duy <duyme18@gmail.com>
2020-12-15 11:38:11 +0700

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../Services/common.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+  public age;
+  constructor(private common: CommonService) {
+    this.age = common.age;
+  }
+  ngOnInit(): void {
+  }
+  public tangTuoi() {
+    this.common.age++;
+    this.age = this.common.age;
+  }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../Services/common.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  public name = "Duy";
+  public age = 24;
+  public vehicles = ['Toyota', 'Honda'];
+
+  constructor(private common: CommonService) {
+    this.age = common.age;
+  }
+
+  ngOnInit(): void {
+  }
+
+  public tangTuoi() {
+    this.common.age++;
+    this.age = this.common.age;
+  }
+}
